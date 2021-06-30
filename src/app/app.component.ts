467f66d206c7626b8f5b3f5d006e6ec5473108cb
AdrianGarzonPozo <adriangarzonpozo@gmail.com>
2021-06-30 17:17:39 +0200

    Random Pkm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { keys } from './keys';
+import { Pokemon } from './modelo/Pokemon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pokedex-PKM';
+  title: string = 'Pokedex-PKM';
+  pokemon: Pokemon=new Pokemon('','',0,[''],[{}],[{}],[{}]);
+
+  ngOnInit() {
+    this.randomPokemon();
+  }
+
+  async randomPokemon(){
+    if (!localStorage.getItem("pkm")) {
+      const randomNumber = Math.floor(Math.random() * 897) + 1;
+      await fetch(`${keys.api}pokemon/${randomNumber}`)
+        .then(response => response.json())
+        .then(data => this.fillPokemon(data))
+        .catch(error=> this.randomPokemon());
+    }else{
+      console.log(this.pokemon);
+    }
+  }
+
+  fillPokemon(pokemon){
+    this.pokemon._id=pokemon.id; 
+    this.pokemon.name=pokemon.name;
+    this.pokemon.weight=pokemon.weight;
+    this.pokemon.types=pokemon.types;
+    this.pokemon.stats=pokemon.stats;
+    this.pokemon.abilities=pokemon.abilities;
+    this.pokemon.sprites=pokemon.sprites;
+    console.log(this.pokemon);
+  }
 }
---
+++ src/app/keys.ts
@@ -0,0 +1,3 @@
+export const keys = {
+    "api":"https://pokeapi.co/api/v2/"
+}
